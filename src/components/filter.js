88e253b83559d2391e7b6525633d0b86bd3920f7
Olya <osokolova1@gmail.com>
2020-04-17 16:49:10 +0300

    Моки для задач

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,24 +1,24 @@
 const createFilterMarkup = (filter, isChecked) => {
-  return (`<input
-        type="radio"
-        id="filter__${name}"
-        class="filter__input visually-hidden"
-        name="filter"
-        ${isChecked ? `checked` : ``}
-      />
-      <label for="filter__${name}" class="filter__label">
-      ${name} <span class="filter__${name}-count">${count}</span></label
-      >`
+  const {title, count} = filter;
+
+  return (
+    `<input
+      type="radio"
+      id="filter__${title}"
+      class="filter__input visually-hidden"
+      name="filter"
+      ${isChecked ? `checked` : ``}
+    />
+    <label for="filter__${title}" class="filter__label">
+      ${title} <span class="filter__${title}-count">${count}</span></label
+    >`
   );
 };
-
 
 export const createFilterTemplate = (filters) => {
   const filtersMarkup = filters.map((it, i) => createFilterMarkup(it, i === 0)).join(`\n`);
 
-  return (
-    `<section class="main__filter filter container">
-       ${filtersMarkup}
-    </section>`
-  );
+  return `<section class="main__filter filter container">
+    ${filtersMarkup}
+  </section>`;
 };
